b9bc21c05e9b1a78e7592800ca7c09a029e9b95e
stas <stasikusk@gmail.com>
2014-10-14 01:29:58 +0300

    g

---
+++ VK_api_img_post/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace VK_api_img_post
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // load_btn
+            // 
+            this.load_btn.Location = new System.Drawing.Point(12, 12);
+            this.load_btn.Name = "load_btn";
+            this.load_btn.Size = new System.Drawing.Size(173, 23);
+            this.load_btn.TabIndex = 0;
+            this.load_btn.Text = "button1";
+            this.load_btn.UseVisualStyleBackColor = true;
+            this.load_btn.Click += new System.EventHandler(this.load_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.load_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load_btn;
+    }
+}
+
---
+++ VK_api_img_post/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet.Net;
+using xNet.Text;
+
+namespace VK_api_img_post
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth(string login, string pass)
+        {
+            using (var req = new HttpRequest())
+            {
+                //req.UserAgent = HttpHelper.FirefoxUserAgent();
+                CookieDictionary cookie = new CookieDictionary(false);
+                req.Cookies = cookie;
+                req.Get(String.Format("https://login.vk.com/?act=login&email={0}&pass={1}", login, pass));
+
+                string data = req.Get("https://oauth.vk.com/authorize?client_id=4588326&scope=notify,friends,photos,status,wall&redirect_uri=http://oauth.vk.com/blank.html&display=touch&response_type=token").ToString();
+                req.AllowAutoRedirect = false;
+                req.Get(data.Substring("<form method=\"post\" action=\"", "\">"));
+
+                char[] symb = { '=', '&' };
+                string[] splitData = req.Response.Location.Split(symb);
+
+                string token = splitData[1];
+                string userID = splitData[5];
+
+                string friendsList = req.Get(String.Format("https://api.vk.com/method/friends.get?user_id={0}&v=5.25&access_token={1}", userID, token)).ToString();
+                
+                MessageBox.Show(token);
+            }
+        }
+
+        private void load_btn_Click(object sender, EventArgs e)
+        {
+            Auth("+380631818725", "qazmko123123123");
+        }
+    }
+}
